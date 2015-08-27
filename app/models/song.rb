e095dd899ad68d23b2e792021e88559bd27042a7
Steven Nunez <steven.nunez@gmail.com>
2015-08-27 18:48:07 -0400

    OMG WORKING

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+  accepts_nested_attributes_for :artist
   extend Slugable
   add_slug_for :name
 end
