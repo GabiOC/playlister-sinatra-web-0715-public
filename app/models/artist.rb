2b640ef01f424bcae96dd2ca729ce899de3d797a
GabiOC <oconnor.gabriele@gmail.com>
2015-08-27 20:55:12 -0400

    all done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,13 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+=begin
+  Extend vs. include:
+  - Extend makes the module or methods available to the class itself
+  - Include makes it available to instances of the class.
+=end
+
   extend Slugable
   add_slug_for :name
 end
